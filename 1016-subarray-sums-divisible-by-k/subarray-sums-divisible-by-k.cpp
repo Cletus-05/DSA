6d1d57139dfc60eeda8c1342643422a932c49b8c
Cletus Fernandes <132187723+Cletus-05@users.noreply.github.com>
2024-11-15 22:44:27 +0530

    Time: 2 ms (84.40%) | Memory: 32.9 MB (91.82%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-    unordered_map<int, int> remainder_count = {{0, 1}}; // To handle cases when the prefix sum is directly divisible by k
-    int prefix_sum = 0;
-    int count = 0;
-    
-    for(int num : nums) {
-        prefix_sum += num;
-        int remainder = prefix_sum % k;
+        vector<int> freq(k,0);
+        int count=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(k!=0)
+            sum=sum%k;
 
-        // Adjust negative remainders to be within [0, k-1]
-        if (remainder < 0) remainder += k;
+            if(sum<0) sum+=k;
 
-        // If this remainder has been seen, it means there's a subarray with a sum divisible by k
-        if (remainder_count.count(remainder)) {
-            count += remainder_count[remainder];
+            if(sum==0) count++;
+            
+            count+=freq[sum];
+            freq[sum]++;
+            
         }
-
-        // Update the frequency of this remainder
-        remainder_count[remainder]++;
-    }
-
-    return count;
+        return count;
     }
 };
