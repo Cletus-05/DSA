bb12c9e76ce0da341cf8a328cbfb383047ec7577
Cletus Fernandes <132187723+Cletus-05@users.noreply.github.com>
2025-06-20 08:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (36.94%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:      
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        if (n == 0 || k == 0) return;
+       reverse(nums.begin(),nums.end()-k);
+       reverse(nums.end()-k,nums.end());
+       reverse(nums.begin(),nums.end());
+    }
+};
